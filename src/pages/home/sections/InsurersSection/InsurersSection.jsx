7e043429219c848215d3654f52f5c4d98ccb3ad1
jdaianova <jdaianova@gmail.com>
2025-07-24 17:21:14 +0200

    insurers

--- src/pages/home/sections/InsurersSection/InsurersSection.jsx
+++ src/pages/home/sections/InsurersSection/InsurersSection.jsx
@@ -1,7 +1,17 @@
+import { insurersData } from "./lib/insurersData"
+
 const InsurersSection = () => {
     return (
-        <div>
-            InsurersSection
+        <div className="flex flex-wrap gap-[2%] justify-center">
+            {insurersData.map((logo) => (
+                <div key={logo.id}
+                    className="flex w-full py-[20px] px-[40px] bg-subtle max-w-[32%] rounded-3xl mb-[2%]">
+                    <img
+                        src={logo.img}
+                        className="w-full"
+                    />
+                </div>
+            ))}
         </div>
     )
 }
---
+++ src/pages/home/sections/InsurersSection/lib/insurersData.js
@@ -0,0 +1,32 @@
+import logoCPZP from '../../../../../shared/assets/svg/cpzp.svg';
+import logoVoZP from '../../../../../shared/assets/svg/vozp.svg';
+import logoVZP from '../../../../../shared/assets/svg/vzp.svg';
+import logoZPMV from '../../../../../shared/assets/svg/zpmv.svg';
+import logoOZP from '../../../../../shared/assets/svg/ozp.svg';
+
+export const insurersData = [
+    {
+        id: 'ceska-prumyslova-zdravotni-pojistovna',
+        img: logoCPZP,
+
+    },
+    {
+        id: 'vojenska-zdravotni-pojistovna',
+        img: logoVoZP,
+
+    },
+    {
+        id: 'vseobecna-zdravotni-pojistovna-ceske-republiky',
+        img: logoVZP,
+
+    },
+    {
+        id: 'zdravotni-pojistovna-ministerstva-vnitra-cr',
+        img: logoZPMV,
+
+    },
+    {
+        id: 'ozp-zdravotni-pojistovna',
+        img: logoOZP,
+    },
+];
